280fd3146d409faee7953db721ef8e45f04e59b7
Neeharika Baral <neehabaral72@gmail.com>
2024-02-23 00:11:50 -0500

    implement back function using history state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,11 +2,23 @@ import { useState } from "react";
 
 function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
   function transition(newMode) {
-    setMode(newMode)
+    setHistory((prev) => [...prev, newMode])
   }
-  return { mode, transition };
+
+  /* goals: 
+  // need to remove the last item in history array 
+    eg. history = [1, 2, 3] -> history.slice = [1, 2] -> create duplicate of this with [...history]
+  // need to set mode to that new last item in history array (eg. new history = [1, 2] -> mode = 2) 
+  */
+
+  function back() {
+    setHistory(prev => [...prev.slice(0, prev.length - 1)])
+  }
+
+  return { mode: history[history.length -1], transition, back };
 }
 
 export default useVisualMode;
